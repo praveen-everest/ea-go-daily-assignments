c38059250f153e8c6413200be88175a0122dad0b
Praveen Bathula <praveen.bathula@everest.engineering>
2022-12-17 18:20:18 +0530

    adds logic for sin, cos, tan and sqrt

--- day1/calc.go
+++ day1/calc.go
@@ -1,6 +1,10 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math"
+)
 
 func add(x, y int) int {
 	return x + y
@@ -21,4 +25,33 @@ func div(x, y float64) (float64, error) {
 		return 0, DivisionByZeroError
 	}
 	return x / y, nil
+}
+
+const DegreeToRadians = math.Pi / 180
+
+func sin(degree float64) float64 {
+	return math.Sin(DegreeToRadians * degree)
+}
+
+func cos(degree float64) float64 {
+	return math.Cos(DegreeToRadians * degree)
+}
+
+func tan(degree float64) float64 {
+	return math.Tan(DegreeToRadians * degree)
+}
+
+var SqrtForNegativeValueError = errors.New("square root for negative numbers is not allowed")
+
+func sqrt(value float64) (float64, error) {
+	if value < 0 {
+		return 0, SqrtForNegativeValueError
+	}
+	return math.Sqrt(value), nil
+}
+
+func main() {
+	for i := 1; i <= 8; i++ {
+		fmt.Println(math.Sqrt(float64(i)))
+	}
 }
--- day1/calc_test.go
+++ day1/calc_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 )
 
@@ -18,7 +19,7 @@ func TestSuccessfulAddition(t *testing.T) {
 	} {
 		result := add(tc.first, tc.second)
 		if result != tc.want {
-			t.Errorf("addition of %d and %d should be %d.", tc.first, tc.second, tc.want)
+			t.Errorf("addition of %d and %d should be %d but got %d.", tc.first, tc.second, tc.want, result)
 		}
 	}
 }
@@ -31,7 +32,7 @@ func TestSuccessfulSubtraction(t *testing.T) {
 	} {
 		result := sub(tc.first, tc.second)
 		if result != tc.want {
-			t.Errorf("subtraction of %d and %d should be %d.", tc.first, tc.second, tc.want)
+			t.Errorf("subtraction of %d and %d should be %d but got %d.", tc.first, tc.second, tc.want, result)
 		}
 	}
 }
@@ -44,7 +45,7 @@ func TestSuccessfulMultiplication(t *testing.T) {
 	} {
 		result := mul(tc.first, tc.second)
 		if result != tc.want {
-			t.Errorf("multiplication of %d and %d should be %d.", tc.first, tc.second, tc.want)
+			t.Errorf("multiplication of %d and %d should be %d but got %d.", tc.first, tc.second, tc.want, result)
 		}
 	}
 }
@@ -57,7 +58,7 @@ func TestSuccessfulDivision(t *testing.T) {
 	} {
 		result, _ := div(tc.first, tc.second)
 		if result != tc.want {
-			t.Errorf("division of %f and %f should be %f.", tc.first, tc.second, tc.want)
+			t.Errorf("division of %f and %f should be %f but got %f.", tc.first, tc.second, tc.want, result)
 		}
 	}
 }
@@ -66,5 +67,85 @@ func TestDivisionWithZeroShouldReturnError(t *testing.T) {
 	_, err := div(10, 0)
 	if err != DivisionByZeroError {
 		t.Errorf("should not perform division with zero.")
+	}
+}
+
+func TestSin(t *testing.T) {
+	for _, tc := range []struct {
+		degree   float64
+		expected float64
+	}{
+		{0, 0},
+		{30, 0.5},
+		{45, 0.707},
+		{60, 0.866},
+		{90, 1},
+	} {
+		value := sin(tc.degree)
+		if math.Abs(tc.expected-value) > 1e-3 {
+			t.Errorf("sine of %f degrees should be %f but got %f", tc.degree, tc.expected, value)
+		}
+	}
+}
+
+func TestCos(t *testing.T) {
+	for _, tc := range []struct {
+		degree   float64
+		expected float64
+	}{
+		{0, 1},
+		{30, 0.866},
+		{45, 0.707},
+		{60, 0.5},
+		{90, 0},
+	} {
+		value := cos(tc.degree)
+		if math.Abs(tc.expected-value) > 1e-3 {
+			t.Errorf("cosine of %f degrees should be %f but got %f.", tc.degree, tc.expected, value)
+		}
+	}
+}
+
+func TestTan(t *testing.T) {
+	for _, tc := range []struct {
+		degree   float64
+		expected float64
+	}{
+		{0, 0},
+		{30, 0.577},
+		{45, 1},
+		{60, 1.732},
+	} {
+		value := tan(tc.degree)
+		if math.Abs(tc.expected-value) > 1e-3 {
+			t.Errorf("tan of %f degrees should be %f but got %f.", tc.degree, tc.expected, value)
+		}
+	}
+}
+
+func TestSqrt(t *testing.T) {
+	for _, tc := range []struct {
+		in       float64
+		expected float64
+	}{
+		{0, 0},
+		{2, 1.414},
+		{16, 4},
+		{625, 25},
+	} {
+		value, err := sqrt(tc.in)
+		if math.Abs(value-tc.expected) > 1e-3 {
+			t.Errorf("square root of %f should be %f but got %f.", tc.in, tc.expected, value)
+		}
+		if err != nil {
+			t.Errorf("should not return error for valid input.")
+		}
+	}
+}
+
+func TestSqrtWithNegativeShouldReturnAnError(t *testing.T) {
+	_, err := sqrt(-1)
+	if err != SqrtForNegativeValueError {
+		t.Errorf("should not calculate square root for negative numbers")
 	}
 }
